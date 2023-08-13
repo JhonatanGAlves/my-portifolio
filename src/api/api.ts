5f61e4ec360485e8e0dcf021a2a7ecb65de61ad9
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-08-13 15:55:25 -0300

    api: 🌐 Getting tools from each project and adding to projects list

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,3 +5,7 @@ const baseUrl = "https://api.github.com/users/JhonatanGAlves/repos";
 export function getRepos(): Promise<any> {
   return axios.get(baseUrl).then((response) => response.data);
 }
+
+export function getTools(tools_url: string | undefined): Promise<any> {
+  return axios.get(tools_url).then((response) => response.data);
+}
--- src/hooks/useProjects.tsx
+++ src/hooks/useProjects.tsx
@@ -1,13 +1,46 @@
 "use-client";
 import { useEffect, useState } from "react";
-import { getRepos } from "@/api/api";
+
+import { getTools, getRepos } from "@/api/api";
 import { ProjectTypes } from "@/types/types";
 
 export default function useProjects() {
   const [projects, setProjects] = useState<ProjectTypes[]>([]);
 
+  async function getReposAndMountTheObject() {
+    const repositories = await getRepos();
+
+    const newRepos = await Promise.all(
+      repositories.map(
+        async ({
+          id,
+          name,
+          description,
+          html_url,
+          created_at,
+          languages_url,
+          topics,
+        }: ProjectTypes) => {
+          const tools = await getTools(languages_url);
+
+          return {
+            id,
+            name,
+            description,
+            html_url,
+            created_at,
+            tools,
+            topics,
+          };
+        }
+      )
+    );
+
+    setProjects(newRepos);
+  }
+
   useEffect(() => {
-    getRepos().then(setProjects);
+    getReposAndMountTheObject();
   }, []);
 
   return { projects };
