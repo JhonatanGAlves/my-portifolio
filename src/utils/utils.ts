b3b4c165cbf580d535668eaf789525eabec09bcf
JhonatanGAlves <jhonatangalves96@gmail.com>
2023-08-13 15:58:38 -0300

    feat: 🧑🏻‍💻 Creating function to handle with percentage and color of each tool

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { ToolsType } from "@/types/types";
+
 export function getExperienceAndWidth(initialDate: Date): {
   experience: number;
   width: number;
@@ -22,4 +24,71 @@ export function getExperienceAndWidth(initialDate: Date): {
   };
 
   return experienceAndWidth;
+}
+
+export function handleToolStatus(
+  allTools: string[],
+  tools: ToolsType,
+  tool: string
+): { tool: string; color: string; percentage: string } {
+  let sum = 0;
+
+  for (let i = 0; i < allTools.length; i++) {
+    sum += tools[allTools[i]];
+  }
+
+  let toolStatus = { tool: "", color: "white", percentage: "" };
+
+  if (allTools.length > 0) {
+    const percentage = ((tools[tool] / sum) * 100).toFixed(1);
+
+    switch (tool) {
+      case "TypeScript":
+        toolStatus = {
+          tool: "TypeScript",
+          color: "#007acc",
+          percentage: percentage,
+        };
+        break;
+      case "JavaScript":
+        toolStatus = {
+          tool: "JavaScript",
+          color: "#f1e05a",
+          percentage: percentage,
+        };
+        break;
+      case "HTML":
+        toolStatus = {
+          tool: "HTML",
+          color: "#e34c26",
+          percentage: percentage,
+        };
+        break;
+      case "CSS":
+        toolStatus = {
+          tool: "CSS",
+          color: "#563D7C",
+          percentage: percentage,
+        };
+        break;
+      case "SCSS":
+        toolStatus = {
+          tool: "SCSS",
+          color: "#C6538C",
+          percentage: percentage,
+        };
+        break;
+      case "PHP":
+        toolStatus = {
+          tool: "PHP",
+          color: "#4F5D95",
+          percentage: percentage,
+        };
+        break;
+      default:
+        break;
+    }
+  }
+
+  return toolStatus;
 }
